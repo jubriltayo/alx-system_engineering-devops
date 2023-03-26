53a034733fbc2d4e9f71bc70eba9a7ae8ca274e0
Tayo Jubril <jubriltayo@gmail.com>
2023-03-26 17:18:50 +0100

    task 0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# A puppet manifest to create file
+file { '/tmp/school':
+  ensure  => 'present',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
