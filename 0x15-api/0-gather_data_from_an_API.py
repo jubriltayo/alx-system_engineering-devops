4aeeac54191f104c6b6e7a28ef774447d7f974e2
Tayo Jubril <jubriltayo@gmail.com>
2023-04-29 18:19:01 +0100

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """
-A script that returns information
-about a TODO list progress using 
-REST API for a given employee ID
+returns information
+about a TODO list
+progress using
+REST API for a
+given employee ID
 """
 import requests
 import sys
