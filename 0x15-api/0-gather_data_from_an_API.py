d47c01d3a5fc057c09e1f2e042270a65a820121a
Tayo Jubril <jubriltayo@gmail.com>
2023-04-29 18:32:14 +0100

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
 """
-Prints employee TODO list info from REST API
+This script prints employee TODO list information from a REST API.
 """
 
 import requests
 import sys
 
+
 if __name__ == "__main__":
-    """ Username """
+    """Get employee name"""
     url = "https://jsonplaceholder.typicode.com"
     user_id = sys.argv[1]
     user_ep = "{}/users/{}".format(url, user_id)
     name = requests.get(user_ep).json().get("name")
-    """ Tasks """
+
+    """Get employee tasks"""
     tasks_ep = "{}/todos".format(url)
     tasks = requests.get(tasks_ep).json()
-    tasks_t = [dict for dict in tasks if dict.get("userId") == int(user_id)]
-    tasks_c = [dict for dict in tasks_t if dict.get("completed")]
+    tasks_t = [d for d in tasks if d.get("userId") == int(user_id)]
+    tasks_c = [d for d in tasks_t if d.get("completed")]
 
     print("Employee {} is done with tasks ({}/{}):"
           .format(name, len(tasks_c), len(tasks_t)))
     for task in tasks_c:
-        print("\t {}".format(task.get("title")))
+        print("\t{}".format(task.get("title")))
